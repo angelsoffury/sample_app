50606b2843cbcac18c33e2a25fbe2678711e4370
angelsoffury <ahalyadas01@gmail.com>
2015-06-25 14:58:14 +0530

    Adding functional destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,12 @@ def edit
       render 'edit'
     end
   end
+
+   def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
   
   private
 
